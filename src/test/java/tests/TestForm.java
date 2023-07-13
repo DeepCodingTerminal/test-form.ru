16d1bc1281a842fa6c82b0bf87e5a0925fb0895b
Testorium <pmichael909@gmail.com>
2023-07-13 22:38:14 +0300

    upd2

--- src/test/java/tests/TestForm.java
+++ src/test/java/tests/TestForm.java
@@ -26,6 +26,13 @@ public class TestForm {
         $x("//input[@name='fname']").setValue("Mihail");
         $x("//input[@name='lname']").setValue("Krylov");
 
+        $x("//div[@data-test-id='birth-date__day']").click();
+        $x("//span[@data-test-id='birth-date__day-value:11']").setValue("10").pressEnter();
+        $x("//div[@data-test-id='birth-date__month']").click();
+        $x("//span[@data-test-id='birth-date__month-value:11']").selectOptionByValue("10");
+        $x("//div[@data-test-id='birth-date__year']").click();
+        $x("//span[@data-test-id='birth-date__year-value:11']").selectOptionByValue("1990");
+        $x("//input[@value='male']").click();
 
 
     }
